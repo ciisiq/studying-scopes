4cb7ed1b6eb6eba5df4fc584e943748318b26acb
ciisiq <cii_3@hotmail.com>
2022-09-23 17:14:51 +0100

    this keyword practice

--- script.js
+++ script.js
@@ -2,45 +2,45 @@
 //Scoping practice
 
 //Global Scope
-function calcAge(birthYear) {
-  //Variable scope
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//Variable scope
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    //block scope
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = 'Steven';
-      //Will use first this firstName than the Global because use the stack chain;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//block scope
+// if (birthYear >= 1981 && birthYear <= 1996) {
+//   var millenial = true;
+//   const firstName = 'Steven';
+//Will use first this firstName than the Global because use the stack chain;
+//   const str = `Oh, and you're a millenial, ${firstName}`;
+//   console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
+//   function add(a, b) {
+//     return a + b;
+//   }
+// }
 
-    // console.log(str);
-    //will give str is not defined because it is a scope only avaliable inside their scope
+// console.log(str);
+//will give str is not defined because it is a scope only avaliable inside their scope
 
-    console.log(millenial);
-    //will find this because is not variable Let or const (ES6) it is VAR
+// console.log(millenial);
+//will find this because is not variable Let or const (ES6) it is VAR
 
-    // console.log(add(2, 3));
-    //will give a error because is not in the same scope, but only because the USE Strict in the top of the page, without it, it will works.
-  }
+// console.log(add(2, 3));
+//will give a error because is not in the same scope, but only because the USE Strict in the top of the page, without it, it will works.
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
 //Global Variable
-const firstName = 'Cintia';
-calcAge(1991);
+// const firstName = 'Cintia';
+// calcAge(1991);
 
 //95 Hoisting and TDZ in practice
 // console.log(me);
@@ -50,11 +50,11 @@ calcAge(1991);
 // console.log(year);
 // Cannot acces "job" before initialization
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
 
-console.log(addDecl(2, 1));
+// console.log(addDecl(2, 1));
 //Will be able to acess
 // console.log(addExpr(2, 1));
 //Will not be able before initilization
@@ -62,12 +62,43 @@ console.log(addDecl(2, 1));
 //Will not be able before initilization
 
 //function declaration
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-const addExpr = function (a, b) {
-  return a + b;
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+//This keyword: this =  window
+//regular function call
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
 };
 
-const addArrow = (a, b) => a + b;
+calcAge(1991);
+// don't show window just the undefined
+//array function
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+// show the window
+
+//inside the method
+const jonas = {
+  name: 'Jonas',
+  year: 1989,
+  calcAge: function () {
+    console.log(this);
+    //show the jonas object window
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
