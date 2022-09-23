f8f7642d8e614e44122d0f8777d39098ff3a7450
ciisiq <cii_3@hotmail.com>
2022-09-23 11:47:57 +0100

    scoping in practice

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+//Scoping practice
+
+//Global Scope
+function calcAge(birthYear) {
+  //Variable scope
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+//Global Variable
+const firstName = 'Cintia';
+calcAge(1991);
